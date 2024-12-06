9a09b572482e58e36a5060efd57622dd872f88eb
HEX9CF <hex9cf@aliyun.com>
2024-12-06 11:08:41 +0800

    Add utils

---
+++ utils/misc.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"encoding/json"
+	"os"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func PrettyStruct(data interface{}) (string, error) {
+	val, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		return "", err
+	}
+	return string(val), nil
+}
+
+func IsFileExists(filePath string) (bool, error) {
+	_, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, err
+		}
+		return false, err
+	}
+	return true, nil
+}
