42e26c730f39002900789207efae497838830668
HEX9CF <hex9cf@aliyun.com>
2024-12-27 12:32:36 +0800

    Add translate instruction model

---
+++ internal/model/translate.go
@@ -0,0 +1,15 @@
+package model
+
+// 翻译题目说明
+type TranslateInstruction struct {
+	Title        string   `json:"title,omitempty" binding:"omitempty"`
+	Description  string   `json:"description,omitempty" binding:"omitempty"`
+	Input        string   `json:"input,omitempty" binding:"omitempty"`
+	Output       string   `json:"output,omitempty" binding:"omitempty"`
+	SampleInput  string   `json:"sample_input,omitempty" binding:"omitempty"`
+	SampleOutput string   `json:"sample_output,omitempty" binding:"omitempty"`
+	Hint         string   `json:"hint,omitempty" binding:"omitempty"`
+	Tags         []string `json:"tags,omitempty" binding:"omitempty"`
+	SourceLang   string   `json:"source_lang,omitempty" binding:"omitempty"`
+	TargetLang   string   `json:"target_lang,omitempty" binding:"omitempty"`
+}
