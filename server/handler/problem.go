48fbca7e63ba8f2126240abdf47899367054dac2
HEX9CF <hex9cf@aliyun.com>
2025-01-07 17:06:34 +0800

    Add parse handler

--- server/handler/problem.go
+++ server/handler/problem.go
@@ -42,8 +42,30 @@ func TranslateProblem(c *gin.Context) {
 		return
 	}
 
-	// 生成题目
+	// 翻译题目
 	p, err := problem.Translate(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
+		return
+	}
+
+	c.JSON(http.StatusOK, model.RespOk("OK", p))
+}
+
+// 解析题目
+func ParseProblem(c *gin.Context) {
+	var req model.ProblemData
+
+	// 参数绑定
+	err := c.ShouldBindBodyWithJSON(&req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+
+	// 解析题目
+	p, err := problem.Parse(req)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
 		return
