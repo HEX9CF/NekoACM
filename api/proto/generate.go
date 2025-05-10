34bac76513cf2225a4eb035309b2c16a5f41a26f
HEX9CF <hex9cf@aliyun.com>
2025-05-10 22:15:07 +0800

    Add debugging output to go generate step and update proto file patterns

--- api/proto/generate.go
+++ api/proto/generate.go
@@ -1,4 +1,4 @@
 package proto
 
-//go:generate protoc --go_out=../../pkg/pb --go_opt=paths=source_relative ./*.proto
-//go:generate protoc --go-grpc_out=../../pkg/pb --go-grpc_opt=paths=source_relative ./*.proto
+//go:generate protoc --go_out=../../pkg/pb --go_opt=paths=source_relative *.proto
+//go:generate protoc --go-grpc_out=../../pkg/pb --go-grpc_opt=paths=source_relative *.proto
