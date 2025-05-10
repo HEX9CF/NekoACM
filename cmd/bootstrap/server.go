13a3ca0917c94cdb78f1744e20d7aa43e443d633
HEX9CF <hex9cf@aliyun.com>
2025-05-10 20:41:41 +0800

    Add concurrent initialization for gRPC and HTTP servers with signal handling

--- cmd/bootstrap/server.go
+++ cmd/bootstrap/server.go
@@ -2,17 +2,48 @@ package bootstrap
 
 import (
 	"log"
+	"neko-acm/internal/interfaces/http"
 	"neko-acm/internal/interfaces/rpc"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // 初始化服务器
 func InitServer() error {
-	err := rpc.InitServer()
-	if err != nil {
-		log.Println("初始化服务器失败！")
-		return err
-	}
+	// 创建错误通道
+	errChan := make(chan error, 2)
+	sigChan := make(chan os.Signal, 1)
 
-	log.Println("初始化服务器成功")
-	return nil
+	// 监听系统信号
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// 启动gRPC服务器
+	go func() {
+		log.Println("正在启动gRPC服务器...")
+		err := rpc.InitServer()
+		if err != nil {
+			log.Println("初始化gRPC服务器失败！")
+			errChan <- err
+		}
+	}()
+
+	// 启动HTTP服务器
+	go func() {
+		log.Println("正在启动HTTP服务器...")
+		err := http.InitServer()
+		if err != nil {
+			log.Println("初始化HTTP服务器失败！")
+			errChan <- err
+		}
+	}()
+
+	// 阻塞主线程，等待错误或终止信号
+	select {
+	case err := <-errChan:
+		return err
+	case sig := <-sigChan:
+		log.Printf("接收到信号: %s，正在关闭服务器...\n", sig)
+		return nil
+	}
 }
