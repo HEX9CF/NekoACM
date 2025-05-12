d7d9ab55bbe896cd2d689258949ff90b4160c4d1
HEX9CF <hex9cf@aliyun.com>
2025-05-12 11:05:58 +0800

    Update Go version to 1.24 and streamline go generate step in build workflow

--- api/proto/generate.go
+++
@@ -1,4 +0,0 @@
-package proto
-
-//go:generate bash -c "protoc --go_out=../../pkg/pb --go_opt=paths=source_relative *.proto"
-//go:generate bash -c "protoc --go-grpc_out=../../pkg/pb --go-grpc_opt=paths=source_relative *.proto"
---
+++ api/proto/proto.go
@@ -0,0 +1,3 @@
+package proto
+
+//go:generate go run ../../dev/gen/pb_gen.go
