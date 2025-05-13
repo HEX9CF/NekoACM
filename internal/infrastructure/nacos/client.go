16987efe8eae1830bcac3fe8a65beeb75063e0c3
HEX9CF <hex9cf@aliyun.com>
2025-05-13 21:00:04 +0800

    Refactor Nacos configuration and registration logic; move initConfig to client.go and create separate register function in naming.go

--- internal/infrastructure/nacos/client.go
+++ internal/infrastructure/nacos/client.go
@@ -4,13 +4,43 @@ import (
 	"github.com/nacos-group/nacos-sdk-go/v2/clients"
 	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
 	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"nekoacm-server/pkg/config"
 )
 
 var (
-	namingClient naming_client.INamingClient
-	configClient config_client.IConfigClient
+	clientConfig  constant.ClientConfig
+	serverConfigs []constant.ServerConfig
+	namingClient  naming_client.INamingClient
+	configClient  config_client.IConfigClient
 )
+
+func initConfig() {
+	cConf := config.Conf.Nacos.Client
+	sConfs := config.Conf.Nacos.Server
+
+	// 创建clientConfig
+	clientConfig = *constant.NewClientConfig(
+		constant.WithNamespaceId(cConf.NamespaceId),
+		constant.WithTimeoutMs(cConf.TimeoutMs),
+		constant.WithNotLoadCacheAtStart(cConf.NotLoadCacheAtStart),
+		constant.WithLogDir(cConf.LogDir),
+		constant.WithCacheDir(cConf.CacheDir),
+		constant.WithLogLevel(cConf.LogLevel),
+	)
+
+	// 创建serverConfig
+	serverConfigs = make([]constant.ServerConfig, 0, len(sConfs))
+	for _, conf := range sConfs {
+		serverConfigs = append(serverConfigs, *constant.NewServerConfig(
+			conf.IpAddr,
+			conf.Port,
+			constant.WithScheme(conf.Scheme),
+			constant.WithContextPath(conf.ContextPath),
+		))
+	}
+}
 
 func initClient() error {
 	var err error
---
+++ internal/infrastructure/nacos/naming.go
@@ -0,0 +1,28 @@
+package nacos
+
+import (
+	"errors"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"nekoacm-server/pkg/config"
+)
+
+func register() error {
+	conf := config.Conf.Nacos.Register
+
+	success, err := namingClient.RegisterInstance(vo.RegisterInstanceParam{
+		Ip:          conf.Ip,
+		Port:        conf.Port,
+		ServiceName: conf.ServiceName,
+		Weight:      conf.Weight,
+		Enable:      conf.Enable,
+		Healthy:     conf.Healthy,
+	})
+	if err != nil {
+		return err
+	}
+	if !success {
+		return errors.New("Nacos 注册实例失败")
+	}
+
+	return err
+}
