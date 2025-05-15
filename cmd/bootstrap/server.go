d3e6547e8fedcda9ffe5ba75bf32aa95b6209c32
HEX9CF <hex9cf@aliyun.com>
2025-05-15 15:00:35 +0800

    feat: update output messages to display structured data and enhance readability

--- cmd/bootstrap/server.go
+++ cmd/bootstrap/server.go
@@ -2,16 +2,22 @@ package bootstrap
 
 import (
 	"log"
+	"nekoacm-server/internal/infrastructure/nacos"
 	"nekoacm-server/internal/interfaces/rpc"
+	"nekoacm-server/pkg/config"
 )
 
 // 初始化服务器
 func initServer() error {
-	// 启动gRPC服务器
-	log.Println("启动 gRPC 服务器")
+	if config.Conf.Nacos.Enable {
+		if err := nacos.NacosClient.Register(); err != nil {
+			log.Println("注册服务到 Nacos 失败！")
+			return err
+		}
+	}
 
-	err := rpc.InitServer()
-	if err != nil {
+	log.Println("启动 gRPC 服务器")
+	if err := rpc.InitServer(); err != nil {
 		log.Println("初始化 gRPC 服务器失败！")
 		return err
 	}
