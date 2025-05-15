991064f5629f41871905a3faeb9aa5bc9c641aec
HEX9CF <hex9cf@aliyun.com>
2025-05-15 11:25:07 +0800

    feat: initialize Nacos client and load configuration

--- internal/infrastructure/nacos/nacos.go
+++ internal/infrastructure/nacos/nacos.go
@@ -1,12 +1,45 @@
 package nacos
 
+import (
+	"nekoacm-common/infrastructure/nacos"
+	"nekoacm-server/pkg/config"
+)
+
+var (
+	NacosClient *nacos.NacosClient
+)
+
+// InitNacos 初始化Nacos服务
 func InitNacos() error {
+	conf := &config.Conf.Nacos
+
+	// 使用server包中的配置创建nacos客户端
+	NacosClient = nacos.NewNacosClient(conf)
+
+	// 初始化nacos客户端
+	if err := NacosClient.Init(); err != nil {
+		return err
+	}
+
+	// 注册服务
+	if conf.Register.Enable {
+		if err := NacosClient.Register(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func LoadConfig() error {
 	var err error
+	conf := config.Conf.Nacos.Config
 
-	initNacosConfig()
+	if err = NacosClient.GetConfig(&config.Conf.Grpc, conf.GrpcDataId); err != nil {
+		return err
+	}
 
-	err = initClient()
-	if err != nil {
+	if err = NacosClient.GetConfig(&config.Conf.Openai, conf.OpenaiDataId); err != nil {
 		return err
 	}
 
